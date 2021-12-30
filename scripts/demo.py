a027abf078d87a0277dca1cd917774670a9cc1ea
Tim-blo <79997012+Tim-blo@users.noreply.github.com>
2021-12-30 13:13:49 +0100

    Update demo.py

--- scripts/demo.py
+++ scripts/demo.py
@@ -22,6 +22,7 @@ import cv2
 import time
 import joblib
 import argparse
+import glob
 from loguru import logger
 
 sys.path.append('.')
@@ -38,56 +39,38 @@ MIN_NUM_FRAMES = 0
 
 
 def main(args):
+    
+    tester = PARETester(args)
 
-    demo_mode = args.mode
+    video_folder = args.vid_folder
 
-    if demo_mode == 'video':
-        video_file = args.vid_file
+    # ========= [Optional] download the youtube video ========= #
 
-        # ========= [Optional] download the youtube video ========= #
-        if video_file.startswith('https://www.youtube.com'):
-            logger.info(f'Downloading YouTube video \"{video_file}\"')
-            video_file = download_youtube_clip(video_file, '/tmp')
+    if not os.path.isfile(video_folder):
+        exit(f'Input video folder \"{video_folder}\" does not exist!')
+    
+    scene = args.scene
+    
+    videos = glob.glob(video_folder + '/' + scene + '*.avi')
+    videos.sort()
+    print(len(videos))
+    
+    output_path = os.path.join(args.output_folder)
+    os.makedirs(output_path, exist_ok=True)
 
-            if video_file is None:
-                exit('Youtube url is not valid!')
-
-            logger.info(f'YouTube Video has been downloaded to {video_file}...')
-
-        if not os.path.isfile(video_file):
-            exit(f'Input video \"{video_file}\" does not exist!')
-
-        output_path = os.path.join(args.output_folder, os.path.basename(video_file).replace('.mp4', '_' + args.exp))
-        os.makedirs(output_path, exist_ok=True)
-
-        if os.path.isdir(os.path.join(output_path, 'tmp_images')):
-            input_image_folder = os.path.join(output_path, 'tmp_images')
-            logger.info(f'Frames are already extracted in \"{input_image_folder}\"')
-            num_frames = len(os.listdir(input_image_folder))
-            img_shape = cv2.imread(os.path.join(input_image_folder, '000001.png')).shape
-        else:
-            input_image_folder, num_frames, img_shape = video_to_images(
-                video_file,
-                img_folder=os.path.join(output_path, 'tmp_images'),
-                return_info=True
-            )
-        output_img_folder = f'{input_image_folder}_output'
-        os.makedirs(output_img_folder, exist_ok=True)
-    elif demo_mode == 'folder':
-        args.tracker_batch_size = 1
-        input_image_folder = args.image_folder
-        output_path = os.path.join(args.output_folder, input_image_folder.rstrip('/').split('/')[-1] + '_' + args.exp)
-        os.makedirs(output_path, exist_ok=True)
-
-        output_img_folder = os.path.join(output_path, 'pare_results')
-        os.makedirs(output_img_folder, exist_ok=True)
-
+    if os.path.isdir(os.path.join(output_path, 'tmp_images')):
+        input_image_folder = os.path.join(output_path, 'tmp_images')
+        logger.info(f'Frames are already extracted in \"{input_image_folder}\"')
         num_frames = len(os.listdir(input_image_folder))
-    elif demo_mode == 'webcam':
-        logger.error('Webcam demo is not implemented!..')
-        raise NotImplementedError
+        img_shape = cv2.imread(os.path.join(input_image_folder, '000001.png')).shape
     else:
-        raise ValueError(f'{demo_mode} is not a valid demo mode.')
+        input_image_folder, num_frames, img_shape = video_to_images(
+            video_file,
+            img_folder=os.path.join(output_path, 'tmp_images'),
+            return_info=True
+        )
+    output_img_folder = f'{input_image_folder}_output'
+    os.makedirs(output_img_folder, exist_ok=True)
 
     logger.add(
         os.path.join(output_path, 'demo.log'),
@@ -96,10 +79,9 @@ def main(args):
     )
     logger.info(f'Demo options: \n {args}')
 
-    tester = PARETester(args)
-
-    total_time = time.time()
-    if args.mode == 'video':
+    for idx, video_file in enumerate(videos):
+        print("Applying PARE estimation to video", idx+1, "on", len(videos))
+        total_time = time.time()
         logger.info(f'Input video number of frames {num_frames}')
         orig_height, orig_width = img_shape[:2]
         total_time = time.time()
@@ -110,50 +92,15 @@ def main(args):
 
         fps = num_frames / (end - pare_time)
 
-        del tester.model
-
         logger.info(f'PARE FPS: {fps:.2f}')
         total_time = time.time() - total_time
         logger.info(f'Total time spent: {total_time:.2f} seconds (including model loading time).')
         logger.info(f'Total FPS (including model loading time): {num_frames / total_time:.2f}.')
 
-        if not args.no_save:
-            logger.info(f'Saving output results to \"{os.path.join(output_path, "pare_output.pkl")}\".')
-            joblib.dump(pare_results, os.path.join(output_path, "pare_output.pkl"))
+        logger.info(f'Saving output results to \"{os.path.join(output_path, "pare_output.pkl")}\".')
+        joblib.dump(pare_results, os.path.join(output_path, "pare_output.pkl"))
 
-        if not args.no_render:
-            tester.render_results(pare_results, input_image_folder, output_img_folder, output_path,
-                                  orig_width, orig_height, num_frames)
-
-            # ========= Save rendered video ========= #
-            vid_name = os.path.basename(video_file)
-            save_name = f'{vid_name.replace(".mp4", "")}_{args.exp}_result.mp4'
-            save_name = os.path.join(output_path, save_name)
-            logger.info(f'Saving result video to {save_name}')
-            images_to_video(img_folder=output_img_folder, output_vid_file=save_name)
-
-            # Save the input video as well
-            images_to_video(img_folder=input_image_folder, output_vid_file=os.path.join(output_path, vid_name))
-            # shutil.rmtree(output_img_folder)
-
-        # shutil.rmtree(image_folder)
-    elif args.mode == 'folder':
-        logger.info(f'Number of input frames {num_frames}')
-
-        total_time = time.time()
-        detections = tester.run_detector(input_image_folder)
-        pare_time = time.time()
-        tester.run_on_image_folder(input_image_folder, detections, output_path, output_img_folder)
-        end = time.time()
-
-        fps = num_frames / (end - pare_time)
-
-        del tester.model
-
-        logger.info(f'PARE FPS: {fps:.2f}')
-        total_time = time.time() - total_time
-        logger.info(f'Total time spent: {total_time:.2f} seconds (including model loading time).')
-        logger.info(f'Total FPS (including model loading time): {num_frames / total_time:.2f}.')
+    # shutil.rmtree(image_folder)
 
 
     logger.info('================= END =================')
@@ -170,9 +117,9 @@ if __name__ == '__main__':
 
     parser.add_argument('--exp', type=str, default='',
                         help='short description of the experiment')
-
-    parser.add_argument('--mode', default='video', choices=['video', 'folder', 'webcam'],
-                        help='Demo type')
+    
+    parser.add_argument('--vid_folder', type=str,
+                        help='input folder')
 
     parser.add_argument('--vid_file', type=str,
                         help='input video path or youtube link')
@@ -232,6 +179,9 @@ if __name__ == '__main__':
 
     parser.add_argument('--save_obj', action='store_true',
                         help='save results as .obj files.')
+    
+    parser.add_argument('--scene', type=str,
+                        help='which scene to consider')
 
     args = parser.parse_args()
 
